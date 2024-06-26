263992f0da447b6f0e691d47e8c983d247937586
tito-Saptarshi <saptarshi.tito.20@gmail.com>
2024-06-26 22:41:31 +0530

    settings page updated / updateUsername server action created

--- app/actions.ts
+++ app/actions.ts
@@ -3,8 +3,10 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 import prisma from "./lib/db";
+import { Prisma } from "@prisma/client";
 
-export async function updateUsername(formData: FormData) {
+
+export async function updateUsername(prevState: any, formData: FormData) {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
@@ -27,7 +29,15 @@ export async function updateUsername(formData: FormData) {
       message: "Succesfully Updated name",
       status: "green",
     };
-  } catch (error) {
-    
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      if (e.code === "P2002") {
+        return {
+          message: "This username is alredy used",
+          status: "error",
+        };
+      }
+    }
+    throw e;
   }
 }
--- app/components/SettingsForm.tsx
+++ app/components/SettingsForm.tsx
@@ -7,21 +7,44 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { SubmitButton } from "./SubmitButtons";
+import { useFormState } from "react-dom";
+import { useToast } from "@/components/ui/use-toast"
+import { useEffect } from "react";
 
+const initialState = {
+  message: "",
+  status: "",
+};
 export function SettingsForm({
   username,
 }: {
   username: string | null | undefined;
 }) {
+  const { toast } = useToast();
+  const [state, formAction] = useFormState(updateUsername, initialState);
+  useEffect(() => {
+    if (state?.status === "green") {
+      toast({
+        title: "Succesfull",
+        description: state.message,
+      });
+    } else if (state?.status === "error") {
+      toast({
+        title: "Error",
+        description: state.message,
+        variant: "destructive",
+      });
+    }
+  }, [state, toast]);
+
   return (
-    <form action={updateUsername}>
+    <form action={formAction}>
       <h1 className="text-3xl font-extrabold tracking-tight">Settings</h1>
       <Separator className="my-4" />
       <Label className="text-lg">Username</Label>
       <p className="text-muted-foreground">
         In this Settings page you can change your username!
       </p>
-
       <Input
         defaultValue={username ?? undefined}
         name="username"
@@ -30,6 +53,9 @@ export function SettingsForm({
         min={2}
         maxLength={21}
       />
+       {state?.status === "error" && (
+        <p className="text-destructive mt-1">{state.message}</p>
+      )}
 
       <div className="w-full flex mt-5 gap-x-5 justify-end">
         <Button variant="secondary" asChild type="button">
