60c54aebe768304346681c5afbf36ee67fc17d85
tito-Saptarshi <saptarshi.tito.20@gmail.com>
2024-07-06 02:31:50 +0530

    home c started/ right side - c/h

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,42 @@
+import { Card } from "@/components/ui/card";
 import Image from "next/image";
-
+import Banner from "../public/banner.png";
+import HelloImage from "../public/hero-image.png";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 export default function Home() {
   return (
-    <h1>Reddit</h1> 
+    <div className="max-w-[1000px] mx-auto flex gap-x-10 mt-4 mb-10">
+      <div className="w-[65%] flex flex-col gap-y-5">Reddit</div>
+      <div className="w-[35%]">
+        <Card>
+          <Image src={Banner} alt="Banner" />
+          <div className="p-2">
+            <div className="flex items-center">
+              <Image
+                src={HelloImage}
+                alt="Hello Image"
+                className="w-10 h-16 -mt-6"
+              />
+              <h1 className="font-medium pl-3">Home</h1>
+            </div>
+            <p className="text-sm text-muted-foreground pt-2">
+              Your Home Reddit frontpage. Come here to check in with your
+              favorite communites!
+            </p>
+            <Separator className="my-5" />
+            <div className="flex flex-col gap-y-3">
+              <Button asChild variant="secondary">
+                <Link href="/r/janmarshal/create">Create Post</Link>
+              </Button>
+              <Button asChild>
+                <Link href="/r/create">Create Community</Link>
+              </Button>
+            </div>
+          </div>
+        </Card>
+      </div>
+    </div>
   );
 }
