818190fe83f861e1ced2acfd6a8b446728a77432
tito-Saptarshi <saptarshi.tito.20@gmail.com>
2024-07-25 10:16:03 +0530

    post page -uc

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,8 +1,12 @@
+import { handleVote } from "@/app/actions";
+import { CopyLink } from "@/app/components/CopyLink";
+import { RendertoJson } from "@/app/components/RendertoJson";
+import { DownVote, UpVote } from "@/app/components/SubmitButtons";
 import prisma from "@/app/lib/db";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
-import { Cake } from "lucide-react";
+import { Cake, MessageCircle } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
@@ -21,6 +25,11 @@ async function getData(id: string) {
       textContent: true,
       subName: true,
       id: true,
+      Vote: {
+        select: {
+          voteType: true,
+        },
+      },
       Subreddit: {
         select: {
           name: true,
@@ -48,7 +57,56 @@ export default async function PostPage({ params }: { params: { id: string } }) {
   return (
     <div className="max-w-[1200px] mx-auto flex gap-x-10 mt-4 mb-10">
       <div className="w-[70%] flex flex-col gap-y-5">
-        <h1>{data.title}</h1>
+        <Card className="p-2 flex">
+          <div className="flex flex-col  items-center  gap-y-2  p-2">
+            <form action={handleVote}>
+              <input type="hidden" name="voteDirection" value="UP" />
+              <input type="hidden" name="postId" value={data.id} />
+              <UpVote />
+            </form>
+            {data.Vote.reduce((acc, vote) => {
+              if (vote.voteType === "UP") return acc + 1;
+              if (vote.voteType === "DOWN") return acc - 1;
+              return acc;
+            }, 0)}
+            <form action={handleVote}>
+              <input type="hidden" name="voteDirection" value="DOWN" />
+              <input type="hidden" name="postId" value={data.id} />
+              <DownVote />
+            </form>
+          </div>
+
+          <div className="p-2 w-full">
+            <p className="text-xs text-muted-foreground">
+              Posted by u/{data.User?.userName}
+            </p>
+
+            <h1 className="font-medium mt-1 text-lg">{data.title}</h1>
+
+            {data.imageString && (
+              <Image
+                src={data.imageString}
+                alt="User Image"
+                width={500}
+                height={400}
+                className="w-full h-auto object-contain mt-2"
+              />
+            )}
+
+            {data.textContent && <RendertoJson data={data.textContent} />}
+
+            <div className="flex gap-x-5 items-center mt-3">
+              <div className="flex items-center gap-x-1">
+                <MessageCircle className="h-4 w-4 text-muted-foreground" />
+                <p className="text-muted-foreground font-medium text-xs">
+                  108 Comments
+                </p>
+              </div>
+
+              <CopyLink id={params.id} />
+            </div>
+          </div>
+        </Card>
       </div>
 
       <div className="w-[30%]">
