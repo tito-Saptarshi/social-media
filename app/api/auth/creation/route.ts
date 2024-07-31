8eba78c8b712e26d23da5210140afd30ba043173
tito-Saptarshi <saptarshi.tito.20@gmail.com>
2024-07-31 16:24:11 +0530

    changes -01

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -35,6 +35,6 @@ export async function GET(req: NextRequest) {
   return NextResponse.redirect(
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000/"
-      : "https://reddit-yt-ten.vercel.app/"
+      : "https://reddit-vert.vercel.app/"
   );
 }
